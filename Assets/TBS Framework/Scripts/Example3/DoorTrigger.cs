fd7cf4d81b8307b909e260e24e0cd5b336962b47
MrN83 <31300976+MrN83@users.noreply.github.com>
2017-09-01 17:35:49 +1000

    Changes at the end of the week
    DoorTile and DoorTrigger
    New sample scenes (no correct sprites yet)

---
+++ Assets/TBS Framework/Scripts/Example3/DoorTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : Trigger
+{
+	public GameObject DoorCell;
+	public Sprite NoDoorSprite;
+
+	public override void ApplyEffectOnActivation (Unit target)
+	{
+		MySquare doorTile = DoorCell.GetComponent<MySquare> ();
+		doorTile.IsTaken = false;
+		SpriteRenderer renderer = DoorCell.GetComponent<SpriteRenderer> ();
+		renderer.sprite = NoDoorSprite;
+		this.IsActive = false;
+		this.gameObject.SetActive (false);
+	}
+}
